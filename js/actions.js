6b2f7a22347c1d1a5e915cd275570c80d7adabdf
juan miguel sandria velazquez <jmiguel_san@hotmail.com>
2013-02-20 15:59:31 -0600

    version 3

--- js/actions.js
+++ js/actions.js
@@ -2,12 +2,14 @@
 $(document).ready(function(e) {
     document.addEventListener("deviceready", function(){
 		
-		$( ".btnAccesar" ).bind( "click", function(event, ui) {
+		$( '.btnAccesar' ).bind( "click", function(event, ui) {
 			pgAlert('eureka', 'mensaje');
 		});
 
 		
-		
+		$('#acceso .greenButton').tap(function(){ //Obtener Aceleración actual
+			pgAlert('eureka', 'mensaje');
+		});
 				
 		
 		function accError(err){ //Error
